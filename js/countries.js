57fecd92b228e7f6617cf8e3fb6dcf9d7a33b01d
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-07-10 21:25:31 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,25 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+
+const displayCountries = countries =>{
+    // console.log(countries[0]);
+
+    const countriesHtml = countries.map(country => getCountryHtml(country));
+    // console.log(countriesHtml);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHtml.join(' ');
+} 
+
+
+const getCountryHtml = country => {
+    return`
+        <div class = "country"> 
+            <h2> ${country.name.common}</h2>
+            <img src="${country.flags.png}">
+        </div>
+    `
+}
+loadCountries();
